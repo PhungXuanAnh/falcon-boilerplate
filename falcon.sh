77491c16b535d6b476852e689d74f4feb07505b0
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-12-17 11:30:46 +0700

    add command for migrate db to falcon command

--- falcon.sh
+++ falcon.sh
@@ -4,13 +4,33 @@ export APP_ENV="stag"
 # export APP_ENV="prod"
 
 function start () {
-    source .venv/python3/bin/activate
     gunicorn -b 127.0.0.1:5000 --reload app.main:application
 }
 
 function stop () {
     ps -ef | grep gunicorn | awk '{print $2}' | xargs kill -9
 }
+
+function db() {
+    case "$2" in
+        migrate)
+            if [[ $3 == '' ]]
+            then
+                echo 'Usage: falcon.sh migrate "some messages"'
+                exit 1
+            fi        
+            alembic revision --autogenerate -m $3
+            ;;
+        migrations)
+            alembic upgrade head
+            ;;
+        *)
+        echo "Usage: falcon.sh db {migrate|migrations}"
+        exit 1
+    esac
+}
+
+source .venv/python3/bin/activate
 
 case "$1" in
     start)
@@ -19,7 +39,10 @@ case "$1" in
     stop)
         stop
         ;;
+    db)
+        db $@
+        ;;
     *)
-    echo "Usage: run.sh {start|stop}"
+    echo "Usage: falcon.sh {start|stop|db}"
     exit 1
 esac
